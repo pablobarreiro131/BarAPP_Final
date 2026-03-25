1d264c412d048f6f931064e21d5392f5cb3599d8
pablobarreiro131 <pablobarreiro131@gmail.com>
2026-03-25 20:50:35 +0100

    First commit

---
+++ server/BarAPP/src/test/java/org/pbarreiro/barapp/BarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pbarreiro.barapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
