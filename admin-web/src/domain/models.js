9a7da15e8ff9fc4f8ed199e9408ce340f87ccd69
pablobarreiro131 <pablobarreiro131@gmail.com>
2026-04-26 18:42:01 +0200

    changes web side

--- admin-web/src/domain/models.js
+++ admin-web/src/domain/models.js
@@ -1,34 +1,4 @@
-// src/domain/models.js
-
 export const Rol = {
   ADMIN: 'admin',
   CAMARERO: 'camarero'
 };
-
-/**
- * @typedef {Object} Perfil
- * @property {string} id
- * @property {string} nombre
- * @property {string} rol
- */
-
-/**
- * @typedef {Object} Mesa
- * @property {number} id
- * @property {string} nombre
- * @property {boolean} ocupada
- */
-
-/**
- * @typedef {Object} Categoria
- * @property {number} id
- * @property {string} nombre
- */
-
-/**
- * @typedef {Object} Producto
- * @property {number} id
- * @property {string} nombre
- * @property {number} precio
- * @property {number} categoriaId
- */
